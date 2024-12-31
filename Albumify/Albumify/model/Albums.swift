d08f11fed0a9cbbfc200a00d7d43426ae56cf1bd
haneenmedhat2 <haneenmedhat26@gmail.com>
2024-12-31 14:16:58 -0800

    add needed models

---
+++ Albumify/Albumify/model/Albums.swift
@@ -0,0 +1,14 @@
+//
+//  Albums.swift
+//  Albumify
+//
+//  Created by Haneen Medhat on 31/12/2024.
+//
+
+import Foundation
+
+struct Albums : Codable {
+    let userId : Int?
+    let id: Int?
+    let title: String?
+}
---
+++ Albumify/Albumify/model/Photos.swift
@@ -0,0 +1,15 @@
+//
+//  Photos.swift
+//  Albumify
+//
+//  Created by Haneen Medhat on 31/12/2024.
+//
+
+import Foundation
+
+struct Photos : Codable {
+    let albumId : Int?
+    let id : Int?
+    let title : String?
+    let url : String?
+}
